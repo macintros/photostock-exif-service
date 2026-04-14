b1625ef1e05e6f95e00e1cd3bb31d52385472185
macintros <53509544+macintros@users.noreply.github.com>
2026-04-14 19:33:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,43 @@
-from flask import Flask, request, send_file
+from flask import Flask, request, jsonify
 import subprocess
 import tempfile
 import os
+import base64
 
 app = Flask(__name__)
 
 @app.route('/embed', methods=['POST'])
 def embed_metadata():
-    image = request.files['image']
-    title = request.form.get('title', '')
-    description = request.form.get('description', '')
-    keywords = request.form.get('keywords', '')
-    category = request.form.get('category', '')
-    copyright_text = request.form.get('copyright', '')
+    data = request.get_json(force=True)
+    
+    image_b64 = data.get('image', '')
+    title = data.get('title', '')
+    description = data.get('description', '')
+    keywords = data.get('keywords', '')
+    category = data.get('category', '')
+    copyright_text = data.get('copyright', '')
+    filename = data.get('filename', 'output.jpg')
 
+    image_bytes = base64.b64decode(image_b64)
+    
     with tempfile.NamedTemporaryFile(suffix='.jpg', delete=False) as tmp:
-        image.save(tmp.name)
+        tmp.write(image_bytes)
         tmp_path = tmp.name
 
-    output_path = tmp_path.replace('.jpg', '_out.jpg')
+    output_path = tmp_path + '_out.jpg'
 
     cmd = [
         'exiftool',
         f'-Title={title}',
         f'-Description={description}',
         f'-Keywords={keywords}',
-        f'-Subject={keywords}',
-        f'-Category={category}',
-        f'-Copyright={copyright_text}',
         f'-IPTC:ObjectName={title}',
         f'-IPTC:Caption-Abstract={description}',
         f'-IPTC:Keywords={keywords}',
         f'-XMP:Title={title}',
         f'-XMP:Description={description}',
         f'-XMP:Subject={keywords}',
+        f'-Copyright={copyright_text}',
         '-o', output_path,
         tmp_path
     ]
@@ -41,11 +45,18 @@ def embed_metadata():
     subprocess.run(cmd, check=True)
     os.unlink(tmp_path)
 
-    return send_file(output_path, mimetype='image/jpeg')
+    with open(output_path, 'rb') as f:
+        result_b64 = base64.b64encode(f.read()).decode()
+    os.unlink(output_path)
+
+    return jsonify({
+        'image': result_b64,
+        'filename': filename
+    })
 
 @app.route('/health', methods=['GET'])
 def health():
-    return {'status': 'ok'}
+    return jsonify({'status': 'ok'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
